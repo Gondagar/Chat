f1f46202b7af39052138dba5b548b7ce8f4c7312
Serhey Moskalenko <5611666@gmail.com>
2016-03-13 12:19:01 +0200

    Внесені поправки та підвищена надійність роботи коду.

--- src/client/Client.java
+++ src/client/Client.java
@@ -2,66 +2,83 @@ package client;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 /**
  * Created by Admin on 11.03.2016.
  */
 public class Client {
-
-   Scanner  scanner;
+    String name;
+    Scanner scanner;
     String address;
+    OutputStreamWriter outputStreamWriter;
 
 
+    public void run() {
+        initScanner();
+        label1:
+        System.out.print("Введите адресс сервера");
+        System.out.println();
+        address = scanner.nextLine();
 
-  public   void run(){
-            initScanner();
-            System.out.print("Введите адресс сервера");
-            System.out.println();
-            address = scanner.nextLine();
-
-        try(Socket socket = new Socket(address,777)) {
+        try (Socket socket = new Socket(address, 777)) {
 
 
+            outputStreamWriter = new OutputStreamWriter(socket.getOutputStream(), "utf-8");
 
-            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(socket.getOutputStream(), "utf-8");
-
-            Thread thread = new Thread(new ChatListaner(socket));
+            ChatListaner chatListaner = new ChatListaner(socket);
+            Thread thread = new Thread(chatListaner);
 
             thread.start();
 
-            String message = getName();
+            this.name =  sendClientNameForServer();
+
+            String message;
 
             System.out.println("Введите сообщение. Для  завершения работы введите 'exit'");
-            while (!"exit".equals(message = scanner.nextLine())){
+            while (true) {
 
-                outputStreamWriter.write(message + "\n");
-                outputStreamWriter.flush();
+                message = scanner.nextLine();
+                if("exit".equals(message.toLowerCase())){
+
+                    outputStreamWriter.write(message + "\n");
+                    outputStreamWriter.flush();
+                    outputStreamWriter.close();
+                    scanner.close();
+                    chatListaner.isWork = false;
+                    break;
+                } else {
+
+                    outputStreamWriter.write(message + "\n");
+                    outputStreamWriter.flush();
+                }
 
             }
         } catch (IOException e) {
             System.out.println("Подключение не удалось");
+            this.run();
+
         }
 
     }
-
-    private void initScanner()  {
+    private void initScanner() {
         String property = System.getProperty("os.name");
         char cp = property.charAt(0);
-        if (cp == 'W'){
-            scanner = new Scanner(System.in,"cp866");
+        if (cp == 'W') {
+            scanner = new Scanner(System.in, "cp866");
             System.out.println(System.getProperty("os.name"));
             try {
                 System.setOut(new PrintStream(System.out, true, "cp866"));
             } catch (UnsupportedEncodingException e) {
                 e.printStackTrace();
             }
-        } else if (cp == 'L' ){
-            scanner = new Scanner(System.in,"utf-8");
+        } else if (cp == 'L') {
+            scanner = new Scanner(System.in, "utf-8");
             System.out.println(System.getProperty("os.name"));
-        } else if(cp =='M'){
+        } else if (cp == 'M') {
 
-            scanner = new Scanner(System.in,"utf-8");
+            scanner = new Scanner(System.in, "utf-8");
             System.out.println(System.getProperty("os.name"));
         } else {
 
@@ -71,49 +88,60 @@ public class Client {
         }
     }
 
-    private String getName() {
+    private String sendClientNameForServer() {
         String name;
         System.out.print("Введите Ваше имя ");
         System.out.println();
         name = scanner.nextLine();
+        try {
+            outputStreamWriter.write(name + "\n");
+            outputStreamWriter.flush();
+        } catch (IOException e){
+            System.out.println("Отправка имени не удалась");
+        }
 
 
-        return  name;
+
+        return name;
     }
 
-    class ChatListaner implements Runnable{
+    class ChatListaner implements Runnable {
 
-
+        boolean isWork = true;
 
         Socket socket;
 
         public ChatListaner(Socket socket) {
-            this.socket  = socket;
+            this.socket = socket;
 
 
         }
 
         @Override
         public void run() {
-            try ( Scanner scanner =  new Scanner(socket.getInputStream(),"utf-8")) {
+            try (Scanner scanner = new Scanner(socket.getInputStream(), "utf-8")) {
 
-                while (true){
-                    System.out.println(scanner.nextLine());
+                while (true) {
 
-                    Thread.sleep(100);
+                    if(isWork) {
+                        try {
+                            System.out.println(scanner.nextLine());
+                        } catch (NoSuchElementException e){
+                            System.out.println("До встречи");
+                        }
+
+                    } else {
+                        scanner.close();
+                        socket.close();
+                        break;
+                    }
+
                 }
 
             } catch (IOException e) {
                 System.out.println("Упали в потоке получение данных клиентом");
-            } catch (InterruptedException e) {
-                System.out.println("Досведос");
             }
 
         }
     }
-
-
-
-
-
 }
--- src/server/Server.java
+++ src/server/Server.java
@@ -3,95 +3,100 @@ package server;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
+
 import java.util.Scanner;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
-/**
- * Created by Admin on 11.03.2016.
- */
+
 public class Server {
     int count;
 
     // ArrayList<Connection> connectionsArrayList = new ArrayList<Connection>();
     BlockingQueue<Connection> connectionsArrayList = new LinkedBlockingQueue<Connection>();
 
-  public void run()  {
-      ServerSocket serverSocket;
-    try {
-        serverSocket = new ServerSocket(777);
-        System.out.println("Серверный сокет создан");
+    public void run() {
+        ServerSocket serverSocket;
+        try {
+            serverSocket = new ServerSocket(777);
+            System.out.println("Серверный сокет создан");
 
-        while (true) {
-            Socket newSocket = serverSocket.accept();
-            Runnable runnable = new Connection(newSocket);
-            Thread thread = new Thread(runnable);
-            thread.start();
+            do {
+                Socket newSocket = serverSocket.accept();
+                Connection connection = new Connection(newSocket);
+                Thread thread = new Thread(connection);
+                thread.start();
+                connectionsArrayList.add(connection);
 
+            } while (!connectionsArrayList.isEmpty());
+            serverSocket.close();
+            System.out.println("Сервер настроет на автоотлючение при  потере последнего клиента");
+
+        } catch (IOException e) {
+            System.out.println("Сервер не запустился на нужному порту");
         }
 
-
-        } catch (IOException e){
-        System.out.println("Сервер не запустился на нужному порту");
     }
-
-  }
 
     class Connection implements Runnable {
 
         Socket socket;
-
+        Writer writer;
         String name;
+
         public Connection(Socket socket) {
             this.socket = socket;
-            connectionsArrayList.add(this);
+
         }
 
         @Override
         public void run() {
 
-           try(InputStream inputStream = socket.getInputStream()) {
+            try (InputStream inputStream = socket.getInputStream()) {
 
-               Scanner scanner = new Scanner(inputStream,"utf-8");
-               this.name = scanner.nextLine();
-               for (Connection connection : connectionsArrayList){
+                Scanner scanner = new Scanner(inputStream, "utf-8");
+                String message;
+                this.name = scanner.nextLine();
+                message = "Всем привет :)";
+                sendMessage(message);
 
+                while (socket.isConnected()) {
 
-                   Writer writer =  new OutputStreamWriter(connection.socket.getOutputStream(),"utf-8");
-                   writer.write("К нам присоеденился " + this.name + " \n");
-                   writer.flush();
-
-
-               }
-
-               while (socket.isConnected()){
-                    if (socket.isClosed()){
+                    message = scanner.nextLine();
+                    if ("exit".equals(message)) {
+                        System.out.println(this.name + " покидает нас.");
+                        message =  "отключился.";
+                        sendMessage(message);
                         connectionsArrayList.remove(this);
-                        System.out.println(this.name + " покинул нас. ");
                         break;
                     }
-                   String messeng = scanner.nextLine();
+                    System.out.println("Новое сообщение от " + this.name + ": " + message);
+                    sendMessage(message);
 
-                   System.out.println("Получено новое сообщение от клиента -" + messeng);
-                   for (Connection connection : connectionsArrayList){
+                }
 
+            } catch (IOException e) {
+                System.out.println(Thread.currentThread().toString() + " не инициализировался ");
+            }
 
-                       Writer writer =  new OutputStreamWriter(connection.socket.getOutputStream(),"utf-8");
-                       writer.write(this.name + " - " + messeng + "\n");
-                       writer.flush();
+            System.out.println(Thread.currentThread() + " Потерял своего кліента и это последняя строчка которую он выполняет");
+        }
 
+        private void sendMessage(String messeng) throws IOException {
+            for (Connection connection : connectionsArrayList) {
 
-                   }
-                   System.out.println("По идеи должно быть отправлено всем клиентам ");
+                if (connection.equals(this)) continue;
 
-               }
+                if (connection.socket.isConnected()) {
+                    Writer writer = new OutputStreamWriter(connection.socket.getOutputStream(), "utf-8");
+                    writer.write(this.name + ": " + messeng + "\n");
+                    writer.flush();
+                } else {
+                    System.out.println("Клиент " + connection.name + " не доступен");
+                }
 
-           } catch (IOException e) {
-               System.out.println(Thread.currentThread().toString() + " не инициализировался ");
-           }
-
-
+            }
+            System.out.println("По идеи должно быть отправлено всем клиентам ");
         }
     }
 }
